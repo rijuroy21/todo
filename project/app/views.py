643692f0b66709b317409dc1e57d67b0ab67b947
rijuroy21 <rijumonroy@gmail.com>
2024-11-29 11:29:11 +0530

    file added

---
+++ project/app/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render,redirect
+
+# Create your views here.
+# subject=[]
+# def index(request):
+#     if request.method=='POST':
+#             todo=render.POST['tudo']
+#             slno=len(subject)
+#             subject.append({'sl_no':slno+1,'tudo_sub':todo})
+#             l=len(subject)
+#     return render(request,'index.html',{'subject':subject})
+
+# def tudo_update(request,slno):
+#         for i in subject:
+#                 if i ['sl_no']==slno:
+#                         sub=i['tudo_sub']
+#                         print(sub)
+#         if request.method=='POST'
+#                 tudo_sub=request.POST['tudo']
+#                 for i in tudo_list                    
