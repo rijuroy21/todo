74bc0bd6857c69ac764da0aadbfb863044346404
rijuroy21 <rijumonroy@gmail.com>
2024-12-10 09:08:54 +0530

    files added

--- project/app/urls.py
+++ project/app/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('',views.index),
     path('todo_update/<int:slno>',views.todo_update),
+    path('todo_delete/<int:slno>',views.todo_delete),
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -21,3 +21,17 @@ def todo_update(request,slno):
                 print(True)
                 return redirect(index)
     return render(request,'todo_update.html',{'sub':sub,'slno':slno})
+def todo_delete(request,slno):
+    for i in subject:
+        if i['slno']==slno:
+            sub=i['sub']
+            # print(sub)
+    if request.method=='POST':
+        todo_sub=request.POST['todo']
+        print(todo_sub)
+        for i in subject:
+            if i['slno']==slno:
+                subject.remove(i)
+                print(True)
+                return redirect(index)
+    return render(request,'todo_delete.html',{'sub':sub,'slno':slno})
